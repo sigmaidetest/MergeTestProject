0ad015dece5ce103ac21c00828579a456525646d
sigmaidetest <cehib@balanc3r.com>
2019-04-06 02:26:23 +0530

    commit #04

--- MergeTestProject/lambda.js
+++ MergeTestProject/lambda.js
@@ -1,5 +1,46 @@
-exports.handler = function(event, context, callback) {
-    
-    console.log("Added from the Sigma IDE and committed with message: commit #01");
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk')
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = function (event, context, callback) {
+
+    let name = JSON.parse(event.body).name;
+
+    ddb.put({
+        TableName: 'MergeTestDB',
+        Item: { 'Name': name }
+    }).promise()
+        .then(putRecordData => {
+            let response = {
+                "isBase64Encoded": true,
+                "statusCode": 200,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "*",
+                    "Access-Control-Allow-Headers": "*",
+                    "Access-Control-Expose-Headers": "*"
+                },
+                "body": JSON.stringify({
+					"Code": "Success",
+					"Message": "Name: " + name + " is successfully persisted in database.",
+					"Data": putRecordData
+				}),
+            };
+
+            callback(null, response);
+        })
+        .catch(err => {
+            let response = {
+                "statusCode": err.statusCode,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json"
+                },
+                "body": JSON.stringify({
+                    "Code": err.code,
+                    "Message": err.message
+                }),
+                "isBase64Encoded": false
+            };
+            callback(null, response);
+        });
 }
