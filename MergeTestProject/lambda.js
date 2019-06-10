275ebbe2af1ec3dcea6904f31aadf44f7e8f35c4
sigmaidetest <cehib@balanc3r.com>
2019-06-10 15:59:03 +0530

    staging: changed lambda code

--- MergeTestProject/lambda.js
+++ MergeTestProject/lambda.js
@@ -5,52 +5,49 @@ const testData = require("subDir/test-data.json");
 
 exports.handler = function (event, context, callback) {
 
-    console.log("***", event.body);
+    console.log("*", event.body);
     let name = JSON.parse(event.body).name;
 
     let dataName = testData.find(person => person.name === name);
 
     if (dataName) {
-        putToDB(name);
+        ddb.put({
+            TableName: 'MergeTestDB',
+            Item: { 'Name': name }
+        }).promise()
+            .then(putRecordData => {
+                let response = {
+                    "isBase64Encoded": true,
+                    "statusCode": 200,
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "*",
+                        "Access-Control-Allow-Headers": "*",
+                        "Access-Control-Expose-Headers": "*"
+                    },
+                    "body": JSON.stringify({
+                        "Code": "Success",
+                        "Message": "Name: " + name + " is successfully persisted in database...",
+                        "Data": putRecordData
+                    }),
+                };
+
+                callback(null, response);
+            })
+            .catch(err => {
+                let response = {
+                    "statusCode": err.statusCode,
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*",
+                        "Content-Type": "application/json"
+                    },
+                    "body": JSON.stringify({
+                        "Code": err.code,
+                        "Message": err.message
+                    }),
+                    "isBase64Encoded": false
+                };
+                callback(null, response);
+            });
     }
 }
-
-const putToDB = name =>
-    ddb.put({
-        TableName: 'MergeTestDB',
-        Item: { 'Name': name }
-    }).promise()
-        .then(putRecordData => {
-            let response = {
-                "isBase64Encoded": true,
-                "statusCode": 200,
-                "headers": {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "*",
-                    "Access-Control-Allow-Headers": "*",
-                    "Access-Control-Expose-Headers": "*"
-                },
-                "body": JSON.stringify({
-					"Code": "Success",
-					"Message": "Name: " + name + " is successfully persisted in database...",
-					"Data": putRecordData
-				}),
-            };
-
-            callback(null, response);
-        })
-        .catch(err => {
-            let response = {
-                "statusCode": err.statusCode,
-                "headers": {
-                    "Access-Control-Allow-Origin": "*",
-                    "Content-Type": "application/json"
-                },
-                "body": JSON.stringify({
-                    "Code": err.code,
-                    "Message": err.message
-                }),
-                "isBase64Encoded": false
-            };
-            callback(null, response);
-        });
